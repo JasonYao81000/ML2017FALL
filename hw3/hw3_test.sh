39d3ec51719c9dd9d0e23157d135316bef4ac56c
JasonYao81000 <31233752+JasonYao81000@users.noreply.github.com>
2017-11-17 16:26:11 +0800

    fixed wget

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/apj5e5xxo43h0us/GEN_CNN_BEST.h5?dl=1
+wget -O GEN_CNN_BEST.h5 https://www.dropbox.com/s/apj5e5xxo43h0us/GEN_CNN_BEST.h5?dl=1
 python3 test.py $1 $2 $3
